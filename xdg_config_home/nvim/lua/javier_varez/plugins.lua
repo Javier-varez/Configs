6f8673dc12c0cfcb3d96482743e2642bfb04afde
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-09-01 23:25:46 +0200

    Neovim: Update rust-tools and enable proc-macros

--- xdg_config_home/nvim/lua/javier_varez/plugins.lua
+++ xdg_config_home/nvim/lua/javier_varez/plugins.lua
@@ -166,40 +166,50 @@ end
 
 local rust_tools_opts = {
     tools = { -- rust-tools options
-        -- automatically set inlay hints (type hints)
-        -- There is an issue due to which the hints are not applied on the first
-        -- opened file. For now, write to the file to trigger a reapplication of
-        -- the hints or just run :RustSetInlayHints.
-        -- default: true
+        -- Automatically set inlay hints (type hints)
         autoSetHints = true,
 
-        -- whether to show hover actions inside the hover window
-        -- this overrides the default hover handler
-        -- default: true
-        hover_with_actions = false,
+        -- Whether to show hover actions inside the hover window
+        -- This overrides the default hover handler
+        hover_with_actions = true,
 
         runnables = {
             -- whether to use telescope for selection menu or not
-            -- default: true
             use_telescope = true
 
             -- rest of the opts are forwarded to telescope
         },
 
+        debuggables = {
+            -- whether to use telescope for selection menu or not
+            use_telescope = true
+
+            -- rest of the opts are forwarded to telescope
+        },
+
+        -- These apply to the default RustSetInlayHints command
         inlay_hints = {
+
+            -- Only show inlay hints for the current line
+            only_current_line = false,
+
+            -- Event which triggers a refersh of the inlay hints.
+            -- You can make this "CursorMoved" or "CursorMoved,CursorMovedI" but
+            -- not that this may cause  higher CPU usage.
+            -- This option is only respected when only_current_line and
+            -- autoSetHints both are true.
+            only_current_line_autocmd = "CursorHold",
+
             -- wheter to show parameter hints with the inlay hints or not
-            -- default: true
             show_parameter_hints = true,
 
             -- prefix for parameter hints
-            -- default: "<-"
-            parameter_hints_prefix = "<-",
+            parameter_hints_prefix = "<- ",
 
             -- prefix for all the other hints (type, chaining)
-            -- default: "=>"
-            other_hints_prefix  = "=>",
+            other_hints_prefix = "=> ",
 
-            -- whether to align to the lenght of the longest line in the file
+            -- whether to align to the length of the longest line in the file
             max_len_align = false,
 
             -- padding from the left if max_len_align is true
@@ -210,28 +220,39 @@ local rust_tools_opts = {
 
             -- padding from the right if right_align is true
             right_align_padding = 7,
+
+            -- The color of the hints
+            highlight = "Comment",
         },
 
         hover_actions = {
             -- the border that is used for the hover window
             -- see vim.api.nvim_open_win()
             border = {
-              {"╭", "FloatBorder"},
-              {"─", "FloatBorder"},
-              {"╮", "FloatBorder"},
-              {"│", "FloatBorder"},
-              {"╯", "FloatBorder"},
-              {"─", "FloatBorder"},
-              {"╰", "FloatBorder"},
-              {"│", "FloatBorder"}
+                {"╭", "FloatBorder"}, {"─", "FloatBorder"},
+                {"╮", "FloatBorder"}, {"│", "FloatBorder"},
+                {"╯", "FloatBorder"}, {"─", "FloatBorder"},
+                {"╰", "FloatBorder"}, {"│", "FloatBorder"}
             },
+
+            -- whether the hover action window gets automatically focused
+            auto_focus = false
         }
     },
 
     -- all the opts to send to nvim-lspconfig
     -- these override the defaults set by rust-tools.nvim
     -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-    server = { on_attach = rust_on_attach }, -- rust-analyer options
+    server = {
+        on_attach = rust_on_attach,
+        settings = {
+            rust_analyzer = {
+                procMacro = {
+                    enable = true
+                }
+            }
+        }
+    }, -- rust-analyer options
 }
 
 require('rust-tools').setup(rust_tools_opts)
