41183a19a02af9a6b506dbbb569b9550fbe95edb
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-09-01 23:45:12 +0200

    Neovin: add crates.nvim plugin

--- xdg_config_home/nvim/lua/javier_varez/plugins.lua
+++ xdg_config_home/nvim/lua/javier_varez/plugins.lua
@@ -37,6 +37,7 @@ require('packer').startup(function()
     use 'ThePrimeagen/vim-be-good'
     use { 'glacambre/firenvim', run = ':call firenvim#install(0)' }
     use 'norcalli/nvim-terminal.lua'
+    use 'saecki/crates.nvim'
 end)
 
 require('trim').setup({
@@ -325,4 +326,52 @@ require'compe'.setup {
     vsnip = false;
     ultisnips = false;
   };
+}
+
+require("crates").setup {
+    avoid_prerelease = true, -- don't select a prerelease if the requirement does not have a suffix
+    autoload = true, -- automatically run update when opening a Cargo.toml
+    autoupdate = true, -- atomatically update when editing text
+    loading_indicator = true, -- show a loading indicator while fetching crate versions
+    text = {
+        loading    = "   Loading",
+        version    = "   %s",
+        prerelease = "   %s",
+        yanked     = "   %s",
+        nomatch    = "   No match",
+        update     = "   %s",
+        error      = "   Error fetching crate",
+    },
+    highlight = {
+        loading    = "CratesNvimLoading",
+        version    = "CratesNvimVersion",
+        prerelease = "CratesNvimPreRelease",
+        yanked     = "CratesNvimYanked",
+        nomatch    = "CratesNvimNoMatch",
+        update     = "CratesNvimUpdate",
+        error      = "CratesNvimError",
+    },
+    popup = {
+        autofocus = false, -- focus the versions popup when opening it
+        copy_register = '"', -- the register into which the version will be copied
+        style = "minimal", -- same as nvim_open_win opts.style
+        border = "none", -- same as nvim_open_win opts.border
+        max_height = 30,
+        min_width = 20,
+        text = {
+            title   = "  %s ",
+            version = "   %s ",
+            yanked  = "  %s ",
+        },
+        highlight = {
+            title   = "CratesNvimPopupTitle",
+            version = "CratesNvimPopupVersion",
+            yanked  = "CratesNvimPopupYanked",
+        },
+        keys = {
+            hide = { "q", "<esc>" },
+            select = { "<cr>" },
+            copy_version = { "yy" },
+        },
+    },
 }
