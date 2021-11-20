a3fe058825df487c522690bceae6287c31be9f07
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2021-11-20 23:33:50 +0100

    Neovim: Do not require sumneko language server

--- xdg_config_home/nvim/lua/javier_varez/plugins.lua
+++ xdg_config_home/nvim/lua/javier_varez/plugins.lua
@@ -1,4 +1,3 @@
-
 require('packer').startup(function()
     local use = use
     use 'wbthomason/packer.nvim'
@@ -271,37 +270,40 @@ end
 
 -- Declare the following environment variable in order to use this
 local sumneko_root_path = vim.env.SUMNEKO_LUA_ROOT_PATH
-local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
+if sumneko_root_path ~= nil then
+    local sumneko_binary = sumneko_root_path.."/bin/"..system_name.."/lua-language-server"
 
-require'lspconfig'.sumneko_lua.setup {
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = vim.split(package.path, ';'),
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = {
-          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+    require'lspconfig'.sumneko_lua.setup {
+      cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+      settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+            -- Setup your lua path
+            path = vim.split(package.path, ';'),
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {'vim'},
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = {
+              [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+              [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+            },
+          },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = {
+            enable = false,
+          },
         },
       },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-  on_attach = on_attach
-}
+      on_attach = on_attach
+    }
+
+end
 
 require'compe'.setup {
   enabled = true;
