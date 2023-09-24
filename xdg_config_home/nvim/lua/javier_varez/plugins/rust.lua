19e9b75ef46e09d1ee290ab27e7f4cb993f6d232
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2023-09-24 22:27:33 +0200

    Neovim: rust lsp keybindings

--- xdg_config_home/nvim/lua/javier_varez/plugins/rust.lua
+++ xdg_config_home/nvim/lua/javier_varez/plugins/rust.lua
@@ -11,16 +11,46 @@ return {
         init = function()
             -- Rust tools
             local rust_on_attach = function(a, bufnr)
-                on_attach(a, bufnr)
-
-                -- Rust specific mappings.
                 local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
+                -- Mappings.
                 local opts = { noremap=true, silent=true }
 
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+                buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+                buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+                buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+                buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+                buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+                buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+                buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+                buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+                buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+                buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+                buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+                buf_set_keymap('n', '<space><space>d', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+                buf_set_keymap("n", "<space><space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+
+                -- Rust-specific settings
                 buf_set_keymap('n', 'K', '<Cmd>lua require("rust-tools.hover_actions").hover_actions()<CR>', opts)
                 buf_set_keymap('n', '<space>rr', '<Cmd>lua require("rust-tools.runnables").runnables()<CR>', opts)
                 buf_set_keymap('n', '<space>rc', '<Cmd>lua require("rust-tools.open_cargo_toml").open_cargo_toml()<CR>', opts)
+
+                -- Lsp highlights
+                -- Errors in Red
+                vim.api.nvim_command('hi LspDiagnosticsVirtualTextError guifg=Red ctermfg=Red')
+                -- Warnings in Yellow
+                vim.api.nvim_command('hi LspDiagnosticsVirtualTextWarning guifg=Yellow ctermfg=Yellow')
+                -- Info and Hints in White
+                vim.api.nvim_command('hi LspDiagnosticsVirtualTextInformation guifg=White ctermfg=White')
+                vim.api.nvim_command('hi LspDiagnosticsVirtualTextHint guifg=White ctermfg=White')
+
+                vim.api.nvim_command('hi LspDiagnosticsUnderlineError guifg=NONE ctermfg=NONE cterm=underline gui=underline')
+                vim.api.nvim_command('hi LspDiagnosticsUnderlineWarning guifg=NONE ctermfg=NONE cterm=underline gui=underline')
+                vim.api.nvim_command('hi LspDiagnosticsUnderlineInformation guifg=NONE ctermfg=NONE cterm=underline gui=underline')
+                vim.api.nvim_command('hi LspDiagnosticsUnderlineHint guifg=NONE ctermfg=NONE cterm=underline gui=underline')
+
             end
 
             local rust_analyzer_bin = ''
